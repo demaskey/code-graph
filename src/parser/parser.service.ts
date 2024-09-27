48b73ff042938a6a3e368b0501a21d187120d861
Christoph DeMaskey <2024701+demaskey@users.noreply.github.com>
2024-09-27 16:29:17 -0400

    Make some progress in making a query! YAY!

--- src/parser/parser.service.ts
+++ src/parser/parser.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@nestjs/common'
-import Parser from 'tree-sitter'
+import Parser, { Query, QueryCapture, SyntaxNode } from 'tree-sitter'
 import Java from 'tree-sitter-java'
 
 @Injectable()
@@ -13,11 +13,34 @@ export class ParserService {
 
     parseCode(code: string) {
         const tree = this.parser.parse(code, undefined, { bufferSize: 1024 * 1024 });
+        
+        // return this.printASTNodeInfo(tree.rootNode);
 
-        // const query = new Parser.Query(Java, '@definition.class');
-        // const captures = query.captures(tree.rootNode);
-        // return captures.toString();
+        const query = new Query(Java, '(class_declaration name: (identifier) @name) @definition.class');
+        const captures = query.captures(tree.rootNode);
+        let output = "";
+        for(const cap of captures) {
+            output += "Name: " + cap.name + "\n";
+            output += "Node: " + cap.node + "\n\n";
+        }
+        return output;
+    }
 
-        return tree.rootNode.toString();
+    printASTNodeInfo (rootNode: SyntaxNode) {
+        let output = "Node type: " + rootNode.type + "\n";
+
+        for(const child of rootNode.children) {
+            output += "  - Child node type: " + child.type + "\n";
+
+            if(child.children.length > 0 ) {
+                output += this.printASTNodeInfo(child);
+            } else {
+                if(child.text) {
+                    output += "    - Text Content: " + child.text + "\n";
+                }
+            }
+        }
+
+        return output;
     }
 }
