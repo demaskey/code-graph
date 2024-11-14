b6035217b8027365c27fa90c4b77f0f80f20c792
Christoph DeMaskey <2024701+demaskey@users.noreply.github.com>
2024-11-14 12:56:14 -0500

    This isn't working the way I expected. Saving and taking a different approach.

--- src/components/TreeSitterParser.vue
+++
@@ -1,7 +0,0 @@
-<script setup lang="ts">
-
-</script>
-
-<template>
-  <input type="file" ref="file" @change="parseFile.parse()" />
-</template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,29 @@
 <script setup lang="ts">
-import TreeSitterParser from '@/components/TreeSitterParser.vue';
+import { ref } from 'vue'
+import fs from 'fs'
+import Parser from 'tree-sitter'
+import Java from 'tree-sitter-java'
+
+const doc = ref()
+const file = ref()
+const tree = ref()
+
+function parseFile() {
+  file.value = doc.value.files[0]
+
+  const fileContent = fs.readFileSync(file.value, 'utf8')
+
+  const parser = new Parser()
+  parser.setLanguage(Java)
+
+  const tree = parser.parse(fileContent, undefined, { bufferSize: 1024 * 1024 })
+}
 </script>
 
 <template>
   <main>
-    <TreeSitterParser />
+    <input type="file" ref="doc" @change="parseFile" />
+    <h1>Tree</h1>
+    <div>{{ tree }}</div>
   </main>
 </template>
